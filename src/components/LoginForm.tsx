690cfc2d7aaddf8e9cf4c78517041a6f4df07227
christophecraig <christophe.craig@viacesi.fr>
2024-04-26 22:18:48 +0200

    use env for redirect uri

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card"
 
 export function LoginForm() {
-    const spotifyAuthLink = `https://accounts.spotify.com/authorize?client_id=ad6d0c7508a3452384dd8bd3b661a120&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private+user-read-email+user-library-read+user-library-modify+playlist-read-private+playlist-modify-public+playlist-modify-private+user-top-read+user-read-playback-position+user-read-recently-played+user-follow-read+user-follow-modify+user-read-currently-playing+user-read-playback-state+user-modify-playback-state+ugc-image-upload+streaming+app-remote-control`
+    const spotifyAuthLink = `https://accounts.spotify.com/authorize?client_id=ad6d0c7508a3452384dd8bd3b661a120&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=user-read-private+user-read-email+user-library-read+user-library-modify+playlist-read-private+playlist-modify-public+playlist-modify-private+user-top-read+user-read-playback-position+user-read-recently-played+user-follow-read+user-follow-modify+user-read-currently-playing+user-read-playback-state+user-modify-playback-state+ugc-image-upload+streaming+app-remote-control`
 
   return (
     <Card className="mx-auto max-w-sm">
